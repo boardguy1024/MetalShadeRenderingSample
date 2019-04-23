71ef0be15f55378389e38607bf1799b1cf8e83b8
park kyung suk <boardguy1024@gmail.com>
2019-04-23 23:40:52 +0900

    first commit

---
+++ MetalShadeRenderingSample/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MetalShadeRenderingSample
+//
+//  Created by park kyung suk on 2019/04/23.
+//  Copyright © 2019 park kyung suk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
